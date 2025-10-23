3a3b9d4ea26910b7bb62fda48a8c6bfaea4c2745
Yuliia Mustafina <yuliia@shoutly.com>
2025-10-23 22:20:01 +0200

    Remove unused App.css import

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import './App.css';
 
 function App() {
   return (
